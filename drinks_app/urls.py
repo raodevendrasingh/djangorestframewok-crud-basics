43d735725ffdaf5bf217f0b362adabcf040a0610
Devendra Singh Rao <dsrao1602@gmail.com>
2024-01-10 23:02:00 +0530

    django crud rest api project

---
+++ drinks_app/urls.py
@@ -0,0 +1,11 @@
+from django import views
+from django.urls import path
+from . import views
+from rest_framework.urlpatterns import format_suffix_patterns
+
+urlpatterns = [
+    path('drinks/', views.drink_list),
+    path('drinks/<int:id>', views.drink_detail),
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
